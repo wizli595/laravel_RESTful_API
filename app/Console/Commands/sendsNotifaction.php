bf88ce99ef9c64101c38f5cc10e577311af33e34
wizli595 <abdoowizli@gmail.com>
2023-11-16 19:33:42 +0100

    custom command

---
+++ app/Console/Commands/sendsNotifaction.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class sendsNotifaction extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sends-notifaction';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends notifications to all event atttendes that event starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+        $eventCount = $events->count();
+        $eventLbl = Str::plural('event', $eventCount);
+        $this->info("Found {$eventCount} {$eventLbl}");
+        $events->each(
+            fn ($event) => $event->attendees->each(
+                fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+            )
+        );
+    }
+}
